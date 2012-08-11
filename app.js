d4004fd67ba752c43ac5deb608d2b3ba0c4079fa
Selsie Hanson <code735@gmail.com>
2012-08-11 00:20:12 +0000

    a bit of refactoring

--- app.js
+++ app.js
@@ -2,47 +2,14 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var routes = require('./routes');
 
-var fs = require("fs");
-var projectRoute = require('./routes/project')
+var projectRoute = require('./routes/project');
 var app = module.exports = express.createServer();
-var eeps = require("./lib/ExtJs_helpers/extjs_project_type");
-var path  =  require("path");
 
-var filewalker = require('filewalker');
-var files = []
-var directories = []
 
-// var fs = require('fs');
-var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
-    list.forEach(function(file) {
-      file = dir + '/' + file;
-      fs.stat(file, function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
-        } else {
-          results.push(file);
-          if (!--pending) done(null, results);
-        }
-      });
-    });
-  });
-};
 
-// walk("./projects", function(err, results) {
-//   if (err) throw err;
-//   console.log(results);
-// });
 //Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -79,8 +46,6 @@ app.get('/template', routes.getTemplate);
 app.post('/script', routes.saveScript);
 app.get('/templates', routes.getTemplates);
 
-app.get("/test",projectRoute.tester);
-app.get("/test2",projectRoute.test2)
 //Project
 app.post('/project', projectRoute.createProject);
 app.get('/project/:name?', projectRoute.getProject);
@@ -89,8 +54,6 @@ app.post('/project', projectRoute.updateProject);
 
 app.post('/file',projectRoute.createFile);
 app.get('/file',projectRoute.getFile);
-
-// someStart();
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
